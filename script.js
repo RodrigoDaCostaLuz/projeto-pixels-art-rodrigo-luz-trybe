f30a7b770e195d3419d78d405bf60f9fdbed0e33
Rodrigo Luz <rodrigoluz999@gmail.com>
2022-12-14 20:49:20 -0300

    requisito 6,7,8,9

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Variáveis
+const pixelboard = document.getElementById('pixel-board');
 const coresDaPaleta = document.getElementsByClassName('color');
 coresDaPaleta[0].style.backgroundColor = 'black';
 coresDaPaleta[1].style.backgroundColor = 'yellow';
@@ -13,13 +14,20 @@ function constroiTituloH1() {
   const pegaH1 = document.getElementById('title');
   pegaH1.innerHTML = 'Paleta de Cores';
 }
+
+// const savePaletta = () => {
+//   saveStagePaletta = [];
+//   for (let index = 0; index < coresDaPaleta.length; index += 1) {
+//     saveStagePaletta.push(coresDaPaleta[index].style.backgroundColor);
+//   }
+//   localStorage.setItem('colorPalette', JSON.stringify(saveStagePaletta));
+// };
 function btnrandonColor() {
   const btnRandonColor = document.createElement('button');
   document.body.appendChild(btnRandonColor);
   btnRandonColor.id = 'button-random-color';
   btnRandonColor.innerHTML = 'Cores aleatórias';
 
-  const saveStagePaletta = [];
   btnRandonColor.addEventListener('click', () => {
     for (let index = 1; index < coresDaPaleta.length; index += 1) {
       const r = Math.floor(Math.random() * 255).toString(10);
@@ -27,11 +35,57 @@ function btnrandonColor() {
       const b = Math.floor(Math.random() * 255).toString(10);
       const coresRGB = `rgb(${r},${g},${b})`;
       coresDaPaleta[index].style.backgroundColor = coresRGB;
-      saveStagePaletta.push(coresRGB);
     }
-    localStorage.setItem('colorPalette', JSON.stringify(saveStagePaletta));
   });
 }
+const squarePixel = (value) => {
+  for (let i = 1; i <= value; i += 1) {
+    const pixels = document.createElement('pixel');
+    pixels.className = 'pixel';
+    pixelboard.appendChild(pixels);
+  }
+  localStorage.setItem('squarePx', JSON.stringify(squarePixel));
+};
+squarePixel(25);
+// requisito 8, 9 e 10
+for (let i = 0; i < coresDaPaleta.length; i += 1) {
+  coresDaPaleta[i].addEventListener('click', (cor) => {
+    coresDaPaleta[0].classList.remove('selected');
+    coresDaPaleta[1].classList.remove('selected');
+    coresDaPaleta[2].classList.remove('selected');
+    coresDaPaleta[3].classList.remove('selected');
+    cor.target.classList.add('selected');
+  });
+}
+
+const corSelected = () => {
+  for (let index = 0; index < coresDaPaleta.length; index += 1) {
+    coresDaPaleta[index].addEventListener('click', () => {
+      for (let ind = 0; ind < coresDaPaleta.length; ind += 1) {
+        coresDaPaleta[ind].classList.remove('selected');
+      }
+      coresDaPaleta[index].classList.add = 'selected';
+      coresDaPaleta[index].classList.toggle('selected');
+    });
+  }
+  localStorage.setItem('corSelcted', JSON.stringify(corSelected));
+};
+corSelected();
+
+function pixelToColor() {
+  const tdPixel = document.querySelectorAll('.pixel');
+
+  for (let index = 0; index < tdPixel.length; index += 1) {
+    tdPixel[index].addEventListener('click', (evento) => {
+      const selectedColor = document.querySelector('.selected');
+      const color = selectedColor.getAttribute('style');
+      console.log(color);
+      evento.target.setAttribute('style', color);
+    });
+  }
+  localStorage.setItem('pixelToColor', JSON.stringify(pixelToColor));
+}
+pixelToColor();
 // Executa funções
 window.onload = () => {
   constroiTituloH1();
