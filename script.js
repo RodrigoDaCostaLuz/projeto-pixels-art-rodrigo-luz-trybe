2a1919a24d2a056da2f6fb52996daf476abb028f
Rodrigo Luz <rodrigoluz999@gmail.com>
2022-12-15 10:41:19 -0300

    requisitos 9,10 e 11

--- script.js
+++ script.js
@@ -28,9 +28,58 @@ const constroiQuadroPixel = (value) => {
     pixelboard.appendChild(pixels);
   }
 };
-constroiQuadroPixel(25);
+// nomear
+const removeselected = () => {
+  for (let indice = 0; indice < coresDaPaleta.length; indice += 1) {
+    coresDaPaleta[indice].addEventListener('click', (cor) => {
+      coresDaPaleta[0].classList.remove('selected');
+      coresDaPaleta[1].classList.remove('selected');
+      coresDaPaleta[2].classList.remove('selected');
+      coresDaPaleta[3].classList.remove('selected');
+      cor.target.classList.add('selected');
+    });
+  }
+};
+// Funções: seleciona cor aleatoria na paleta e pinta pixels
+const corSelected = () => {
+  for (let index = 0; index < coresDaPaleta.length; index += 1) {
+    coresDaPaleta[index].addEventListener('click', () => {
+      for (let ind = 0; ind < coresDaPaleta.length; ind += 1) {
+        coresDaPaleta[ind].classList.remove('selected');
+      }
+      coresDaPaleta[index].classList.add = 'selected';
+      coresDaPaleta[index].classList.toggle('selected');
+    });
+  }
+};
+function pixelToColor() {
+  const tdPixel = document.querySelectorAll('.pixel');
 
+  for (let index = 0; index < tdPixel.length; index += 1) {
+    tdPixel[index].addEventListener('click', (evento) => {
+      const selectedColor = document.querySelector('.selected');
+      const color = selectedColor.getAttribute('style');
+      // console.log(color);
+      evento.target.setAttribute('style', color);
+    });
+  }
+}
+// Função que limpa o quadro de pixels
+function clearboard() {
+  const btnClearBoard = document.getElementById('clear-board');
+  btnClearBoard.addEventListener('click', () => {
+    const tdPixel = document.getElementsByClassName('pixel');
+    for (let index = 0; index < tdPixel.length; index += 1) {
+      tdPixel[index].style.backgroundColor = 'white';
+    }
+  });
+}
+// Executa funções
 window.onload = () => {
   geraCoresAleatorias();
-  constroiQuadroPixel();
+  constroiQuadroPixel(25);
+  removeselected();
+  corSelected();
+  pixelToColor();
+  clearboard();
 };
