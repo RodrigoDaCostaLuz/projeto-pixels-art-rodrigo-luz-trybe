24175837b585cb114b95eb6d5e9645562d3b547e
Rodrigo Luz <rodrigoluz999@gmail.com>
2022-12-15 10:21:22 -0300

    resolvido problema, geraCoresAleatorias não estava declarada como função

--- script.js
+++ script.js
@@ -3,22 +3,24 @@ const pixelboard = document.getElementById('pixel-board');
 // const coresAleatorias = document.getElementById('color-palette');
 const coresDaPaleta = document.getElementsByClassName('color');
 const btnRandonColor = document.getElementById('button-random-color');
-// Paleta de Cores
+// Paleta de Cores pré-definida
 coresDaPaleta[0].style.backgroundColor = 'black';
 coresDaPaleta[1].style.backgroundColor = 'yellow';
 coresDaPaleta[2].style.backgroundColor = 'red';
 coresDaPaleta[3].style.backgroundColor = 'green';
 // Função gera cores aletórias
-const geraCoresAleatorias = btnRandonColor.addEventListener('click', () => {
-  for (let index = 1; index < coresDaPaleta.length; index += 1) {
-    const r = Math.floor(Math.random() * 255).toString(10);
-    const g = Math.floor(Math.random() * 255).toString(10);
-    const b = Math.floor(Math.random() * 255).toString(10);
-    const coresRGB = `rgb(${r},${g},${b})`;
-    coresDaPaleta[index].style.backgroundColor = coresRGB;
-  }
-});
-// Constrói quadro de pixels
+const geraCoresAleatorias = () => {
+  btnRandonColor.addEventListener('click', () => {
+    for (let index = 1; index < coresDaPaleta.length; index += 1) {
+      const r = Math.floor(Math.random() * 255).toString(10);
+      const g = Math.floor(Math.random() * 255).toString(10);
+      const b = Math.floor(Math.random() * 255).toString(10);
+      const coresRGB = `rgb(${r},${g},${b})`;
+      coresDaPaleta[index].style.backgroundColor = coresRGB;
+    }
+  });
+};
+// Função constrói quadro de pixels
 const constroiQuadroPixel = (value) => {
   for (let index = 1; index <= value; index += 1) {
     const pixels = document.createElement('pixel');
